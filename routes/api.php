b383116722e37aa713098ad15da8926b7e222bcc
Stroher <vinicius.stroher@rcxit.com.br>
2020-06-12 08:50:08 -0300

    adicionado regras e atualizando controladores

--- routes/api.php
+++ routes/api.php
@@ -16,16 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 //auth
 //admin api token - 8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918 
-
-
-// Route::middleware([CheckAge::class])->group
-
-Route::group(['prefix' => 'v1'], function () {
-
-  Route::middleware('auth:api')->get('/user', function (Request $request) {
-	    return $request->user();
-  });
-  
+Route::group(['prefix' => 'v1','middleware' => 'auth:api'], function () {
+  Route::get('/user', function (Request $request) { return $request->user(); });
   Route::resource('pets', PetController::class);
   Route::resource('medical_consultation', MedicalConsultationController::class);
 });
